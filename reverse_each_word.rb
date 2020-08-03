b07773a52562a648911357579b2de649a8b758d4
Hyeoncheol Jung <joe.hyeoncheol.jung@gmail.com>
2020-08-03 09:23:42 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(string)
 reversed_array = []
 reversed = string.reverse
 array = reversed.split(" ")
-reversed_array.push(array.reverse)
+reversed_array << (array.reverse)
   reversed_word = reversed_array.join(" ")
 reversed_word
 end

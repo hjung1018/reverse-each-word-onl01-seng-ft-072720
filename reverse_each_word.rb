0747af9be15f6286374e1d06a705682f60f87a32
Hyeoncheol Jung <joe.hyeoncheol.jung@gmail.com>
2020-08-03 10:01:40 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,8 +2,8 @@ def reverse_each_word(string)
 reversed_array = []
 reversed = string.reverse
 array = reversed.split(" ")
-reversed_array << (array.reverse)
+reversed_array.push(array.reverse)
+reversed_array.map { | word | word.reverse}
   reversed_word = reversed_array.join(" ")
 reversed_word
 end
-
